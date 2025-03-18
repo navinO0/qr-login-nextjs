52c6076789f891a96c7ace24c5afb8e428960988
navinO0 <6y.navin@gmail.com>
2025-03-19 01:52:21 +0530

    ivoice code has been removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,7 @@
 "use client";
 import useProtectedRoute from "@/core/protectedRoute";
-import {
-  Menubar,
-  MenubarCheckboxItem,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
-  MenubarTrigger,
-} from "@/components/ui/menubar"
-import Qr_gen from "@/core/qr_generator";
 import Header from "@/core/header";
-import { CloseAlert } from "@/core/closeAlert";
-import CreateCustomerForm from "@/core/custumCard";
-import TenantFormCard from "@/core/tenetFormCard";
-import Invoice from "./invoice/page";
-import InvoicePage from "./invoice/invoiceCall";
+
 
 
 
